bfcae70e7f722ab532a141fcc9de6089bb859073
Dan Foley <dan@dan-foley.com>
2020-01-28 22:05:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,7 @@ class Dog
   # name equals method
   # takes in an argument of a dog's name and
   # sets that argument equal to a variable: this_dogs_name
+  # setter
   def name=(dog_name)
     this_dogs_name = dog_name
   end
