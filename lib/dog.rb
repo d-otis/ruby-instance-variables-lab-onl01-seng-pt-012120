197cbb7d308b907ce25d6291f0521fbc697f2a57
Dan Foley <dan@dan-foley.com>
2020-01-28 22:12:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   # sets that argument equal to a variable: this_dogs_name
   # setter (all written like property=...)
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   # this method is responsible for reporting, or reading the dog's name!
